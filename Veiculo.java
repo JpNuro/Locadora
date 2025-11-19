222728597a6c2dcb8c991d609519fd18fc6375d3
Spock <lucasfernandezm09@gmail.com>
2025-11-19 20:05:23 -0300

    corrigido classe veiculos e carro

--- Veiculo.java
+++ Veiculo.java
@@ -1,16 +1,55 @@
 public class Veiculo {
     String modelo;
     double preco;
+    int ano;
+    int kmRodados;
 
-    public void setModelo(String m) {
-        modelo = m + " ";
+    public Veiculo(String modelo, double preco, int ano, int kmRodados) {
+        this.modelo = modelo;
+        this.preco = preco;
+        this.ano = ano;
+        this.kmRodados = kmRodados;
     }
 
-    public void setPreco(double p) {
-        preco = p * 1;
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public int getKmRodados() {
+        return kmRodados;
+    }
+
+    public void setKmRodados(int kmRodados) {
+        this.kmRodados = kmRodados;
+    }
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
     }
 
     public double calcularTotal(int dias) {
-        return preco * dias + 10;
+        return preco * dias;
     }
+
+    @Override
+    public String toString() {
+        return "Veiculo [modelo=" + modelo + ", preco=" + preco + ", ano=" + ano + ", kmRodados=" + kmRodados + "]";
+    }
+
+    
 }
