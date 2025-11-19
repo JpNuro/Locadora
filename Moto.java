f1eccdece142d2410b4a0abc2dd18034b6b2a88f
Donalson-pdjout <holojk9@gmail.com>
2025-11-19 20:20:40 -0300

    Moto corrigida

--- Moto.java
+++ Moto.java
@@ -1,5 +1,29 @@
+
 public class Moto extends Veiculo {
-    public double calcularTotal(int dias) {
-        return preco + dias;
+
+    private String placa;
+
+    public Moto(String modelo, double preco, int ano, int kmRodados, String placa) {
+        super(modelo, preco, ano, kmRodados);
+        this.placa = placa;
     }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    @Override
+    public String toString() {
+        return "Moto [placa= " + placa
+                + ", modelo= " + modelo
+                + ", preco= " + preco
+                + ", ano= " + ano
+                + ", kmRodados= " + kmRodados + "]";
+
+    }
+
 }
